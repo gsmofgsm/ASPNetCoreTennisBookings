748c2e594534c33fd408706436afce5626205188
Qing Ma <gsmofgsm@hotmail.com>
2021-03-07 12:05:30 +0100

    TryAddxxx to skip if already registered

---
+++ src/TennisBookings.Web/Services/AmazingWeatherForcaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using TennisBookings.Web.Domain;
+
+namespace TennisBookings.Web.Services
+{
+    internal class AmazingWeatherForcaster : IWeatherForecaster
+    {
+        public Task<CurrentWeatherResult> GetCurrentWeatherAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/TennisBookings.Web/Startup.cs
+++ src/TennisBookings.Web/Startup.cs
@@ -43,14 +43,16 @@ namespace TennisBookings.Web
             services.Configure<BookingConfiguration>(Configuration.GetSection("CourtBookings"));
 
             services.AddHttpClient<IWeatherApiClient, WeatherApiClient>();
-            //services.AddSingleton<IWeatherForecaster, WeatherForecaster>();
+            services.AddSingleton<IWeatherForecaster, WeatherForecaster>();
+            //services.AddSingleton<IWeatherForecaster, AmazingWeatherForcaster>();  // last registration wins
+            services.TryAddSingleton<IWeatherForecaster, AmazingWeatherForcaster>();  // Try will only add when there is no services registered yet
 
-            // service descriptor examples
-            var serviceDescritor1 = new ServiceDescriptor(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
-            var serviceDescritor2 = ServiceDescriptor.Describe(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
-            var serviceDescritor3 = ServiceDescriptor.Singleton(typeof(IWeatherForecaster), typeof(WeatherForecaster));
-            var serviceDescritor4 = ServiceDescriptor.Singleton<IWeatherForecaster, WeatherForecaster>();
-            services.Add(serviceDescritor1);
+            //// service descriptor examples - should avoid doing so
+            //var serviceDescritor1 = new ServiceDescriptor(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
+            //var serviceDescritor2 = ServiceDescriptor.Describe(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
+            //var serviceDescritor3 = ServiceDescriptor.Singleton(typeof(IWeatherForecaster), typeof(WeatherForecaster));
+            //var serviceDescritor4 = ServiceDescriptor.Singleton<IWeatherForecaster, WeatherForecaster>();
+            //services.Add(serviceDescritor1);
 
             services.TryAddScoped<ILessonBookingService, LessonBookingService>();
             services.TryAddScoped<ICourtService, CourtService>();
